cd14acb5c3b35dc6bfa8a7a076ea8a1111b1771c
YounisGG <xywns003@gmail.com>
2026-01-20 00:16:52 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
-document.querySelector(".add-btn").onclick = () =>{
-  alert("سيتم نقلك لصفحة نشر حساب (قريبًا)");
-};
+function toggleMenu(){
+ document.getElementById("side").classList.toggle("active");
+}
+
+let cards=document.getElementById("cards");
+if(cards){
+ for(let i=1;i<=6;i++){
+  cards.innerHTML+=`
+   <div class="card">
+    <h3>حساب لعبة #${i}</h3>
+    <p>وصف مختصر للحساب</p>
+    <button onclick="location.href='payment.html'">شراء</button>
+   </div>`;
+ }
+}
